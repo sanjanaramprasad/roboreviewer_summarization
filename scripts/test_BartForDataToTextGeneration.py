f4d5f134d0743f93f22fba94d928ab3543d5aaa5
sanjanaramprasad <sanjanaramprasad1@gmail.com>
2021-05-13 20:50:34 -0400

    path for tests

--- scripts/test_BartForDataToTextGeneration.py
+++ scripts/test_BartForDataToTextGeneration.py
@@ -95,11 +95,47 @@ class BartForDataToTextGenerationTester():
 
     def test_encoder_addition(self):
         encoder_outputs_added =  model._get_added_encoder_outputs(self.encoder_outputs_list)
-        print(encoder_outputs_added)
+        print(encoder_outputs_added[0].shape)
      
     def test_attn_masks_OR(self):
-        attention_added = model.__get_attention_masks_OR(self.attn_list)
-        print(attention_added)
+        attention_added = model._get_attention_masks_OR(self.attn_list)
+        print(attention_added.shape)
+
+    def test_model_forward(self):
+        data = next(it)
+        input_ids_col0 = data[0] if len(data) >1 else None
+        attention_mask_col0 = data[1] if len(data) >1 else None
+
+        input_ids_col1 = data[2] if len(data) >3 else None
+        attention_mask_col1 = data[3] if len(data) >3 else None
+
+        input_ids_col2 = data[4] if len(data) >5 else None
+        attention_mask_col0 = data[5] if len(data) >5 else None
+
+        input_ids_col3 = data[6] if len(data) >7 else None
+        attention_mask_col0 = data[7] if len(data) >7 else None
+
+        input_ids_col4 = data[8] if len(data) >9 else None
+        attention_mask_col0 = data[9] if len(data) >9 else None
+
+        
+        outputs = model(
+            input_ids_col0 = input_ids_col0,
+            input_ids_col1 = input_ids_col1,
+            input_ids_col2 = input_ids_col2, 
+            input_ids_col3 = input_ids_col3,
+            input_ids_col4 = input_ids_col4,
+            attention_mask_col0 = attention_mask_col0,
+            attention_mask_col1 = attention_mask_col1,
+            attention_mask_col2 = attention_mask_col2,
+            attention_mask_col3 = attention_mask_col3,
+            attention_mask_col4 = attention_mask_col4,
+        )
+
+        print(outputs)
+
+
+    
 
     
         
