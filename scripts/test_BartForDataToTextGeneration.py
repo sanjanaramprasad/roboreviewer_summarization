8d2323aaa9417d3181313239c611deb019bcf181
sanjanaramprasad <sanjanaramprasad1@gmail.com>
2021-05-13 20:39:38 -0400

    path for tests

--- scripts/test_BartForDataToTextGeneration.py
+++ scripts/test_BartForDataToTextGeneration.py
@@ -31,6 +31,7 @@ class BartForDataToTextGenerationTester():
         self.encoder_col4 = encoder_col4
 
         self.encoder_outputs_list = []
+        self.attn_list = []
         return
 
     def test_encoder0(self):
@@ -41,6 +42,7 @@ class BartForDataToTextGenerationTester():
                                 attention_mask = data[1])
         print(encoder_outputs_col0[0].shape)
         self.encoder_outputs_list.append(encoder_outputs_col0)
+        self.attn_list.append(data[1])
         return
 
     def test_encoder1(self):
@@ -52,6 +54,7 @@ class BartForDataToTextGenerationTester():
                                     attention_mask = data[3])
             print(encoder_outputs_col1[0].shape)
             self.encoder_outputs_list.append(encoder_outputs_col1)
+            self.attn_list.append(data[3])
         return
 
     def test_encoder2(self):
@@ -63,6 +66,7 @@ class BartForDataToTextGenerationTester():
                                     attention_mask = data[5])
             print(encoder_outputs_col2[0].shape)
             self.encoder_outputs_list.append(encoder_outputs_col2)
+            self.attn_list.append(data[5])
         return
 
     def test_encoder3(self):
@@ -74,6 +78,7 @@ class BartForDataToTextGenerationTester():
                                     attention_mask = data[7])
             print(encoder_outputs_col3[0].shape)
             self.encoder_outputs_list.append(encoder_outputs_col3)
+            self.attn_list.append(data[7])
         return
 
     def test_encoder4(self):
@@ -85,13 +90,16 @@ class BartForDataToTextGenerationTester():
                                     attention_mask = data[9])
             print(encoder_outputs_col4[0].shape)
             self.encoder_outputs_list.append(encoder_outputs_col4)
+            self.attn_list.append(data[9])
         return
 
     def test_encoder_addition(self):
-        encoder_outputs_added =  model._get_added_encoder_outputs(self.encoder_list)
+        encoder_outputs_added =  model._get_added_encoder_outputs(self.encoder_outputs_list)
         print(encoder_outputs_added)
      
-        
+    def test_attn_masks_OR(self):
+        attention_added = model.__get_attention_masks_OR(self.attn_list)
+        print(attention_added)
 
     
         
@@ -103,4 +111,5 @@ obj.test_encoder2()
 obj.test_encoder3()
 obj.test_encoder4()
 obj.test_encoder_addition()
+obj.test_attn_masks_OR()
     
