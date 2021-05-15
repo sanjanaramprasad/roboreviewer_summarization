f2bb78a1b6de9fdab3e5f39c66fc6a4acab901ef
sanjanaramprasad <sanjanaramprasad1@gmail.com>
2021-05-15 18:02:24 -0400

    inference

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -248,15 +248,18 @@ class LitModel(pl.LightningModule):
         input_ids = torch.ones((num_beams, 1), device=self.model.device, dtype=torch.long)
         input_ids = input_ids * self.model.config.decoder_start_token_id
 
-        length_penalty = length_penalty if length_penalty is not None else self.config.length_penalty
-        early_stopping = early_stopping if early_stopping is not None else self.config.early_stopping
-
+        stopping_criteria = StoppingCriteriaList()
+        if max_len is not None:
+            stopping_criteria.append(MaxLengthCriteria(max_length=max_len))
+        
         beam_scorer = BeamSearchScorer(
                 batch_size=1,
                 num_beams=num_beams,
                 device=self.model.device,
                 length_penalty=2.0,
                 do_early_stopping=True,
+                stopping_criteria=stopping_criteria,
+                **model_kwargs
             )
         logits_processor = LogitsProcessorList([MinLengthLogitsProcessor(5, eos_token_id=self.model.config.eos_token_id),])
         outputs = self.model.beam_search(input_ids, beam_scorer, logits_processor=logits_processor, **model_kwargs)
