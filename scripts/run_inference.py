16f22606a59d1dec22ac5c18ca24b34d6f0bb2b0
sanjanaramprasad <sanjanaramprasad1@gmail.com>
2021-05-16 14:39:20 -0400

    inference code

--- scripts/run_inference.py
+++ scripts/run_inference.py
@@ -5,10 +5,13 @@ from transformers.models.bart.configuration_bart import BartConfig
 import torch
 import torch.distributed as dist
 from torch.nn import functional as Få
-
+from BartForDataToTextGeneration import BartForDataToText
 from transformers.generation_utils import GenerationMixin
 from run_experiment import LitModel
 from transformers import BartTokenizer
+from Data2TextProcessor_1 import SummaryDataModule
+import argparse
+
 class Data2TextGenerator(GenerationMixin):
 
     def __init__(self, model, tokenizer):
@@ -17,11 +20,38 @@ class Data2TextGenerator(GenerationMixin):
         self.config = self.model.config
         self.device = self.model.device
         #print(self.config.max_length)
+
+    def _prepare_encoder_decoder_kwargs_for_generation(
+        self, input_ids_col0: torch.LongTensor,
+        input_ids_col1: torch.LongTensor,
+        input_ids_col2: torch.LongTensor,
+        input_ids_col3: torch.LongTensor,
+        input_ids_col4: torch.LongTensor, model_kwargs
+    ) -> Dict[str, Any]:
+        if "encoder_outputs" not in model_kwargs:
+            # retrieve encoder hidden states
+            encoder_col0, encoder_col1, encoder_col2, encoder_col3, encoder_col4 = self.model.get_encoders()
+            encoder_kwargs = {
+                argument: value for argument, value in model_kwargs.items() if not argument.startswith("decoder_")
+            }
+            print('ENC KWARGS', encoder_kwargs)
+            if not(input_ids_col0 is None):
+                model_kwargs["encoder_outputs_col0"]: ModelOutput = encoder_col0(input_ids_col0, return_dict=True, **encoder_kwargs)
+            if not(input_ids_col1 is None):
+                model_kwargs["encoder_outputs_col1"]: ModelOutput = encoder_col1(input_ids_col1, return_dict=True, **encoder_kwargs)
+            if not(input_ids_col2 is None):
+                model_kwargs["encoder_outputs_col2"]: ModelOutput = encoder_col2(input_ids_col2, return_dict=True, **encoder_kwargs)
+            if not(input_ids_col3 is None):
+                model_kwargs["encoder_outputs_col3"]: ModelOutput = encoder_col3(input_ids_col3, return_dict=True, **encoder_kwargs)
+            if not(input_ids_col4 is None):
+                model_kwargs["encoder_outputs_col4"]: ModelOutput = encoder_col4(input_ids_col4, return_dict=True, **encoder_kwargs)
+
+
+        return model_kwargs
         
-
-
     def generate(self,
-        input_ids: Optional[torch.LongTensor] = None,
+        batch,
+        input_ids = None,
         max_length: Optional[int] = None,
         min_length: Optional[int] = None,
         do_sample: Optional[bool] = None,
@@ -56,6 +86,22 @@ class Data2TextGenerator(GenerationMixin):
         **model_kwargs, 
     ):
 
+
+        input_ids_col0 = batch[0] if len(batch) >1 else None
+        attention_mask_col0 = batch[1] if len(batch) >1 else None
+
+        input_ids_col1 = batch[2] if len(batch) >3 else None
+        attention_mask_col1 = batch[3] if len(batch) >3 else None
+
+        input_ids_col2 = batch[4] if len(batch) >5 else None
+        attention_mask_col2 = batch[5] if len(batch) >5 else None
+
+        input_ids_col3 = batch[6] if len(batch) >7 else None
+        attention_mask_col3 = batch[7] if len(batch) >7 else None
+
+        input_ids_col4 = batch[8] if len(batch) >9 else None
+        attention_mask_col4 = batch[9] if len(batch) >9 else None
+    
         max_length = max_length if max_length is not None else self.config.max_length
         num_beams = num_beams if num_beams is not None else self.config.num_beams
         num_beam_groups = num_beam_groups if num_beam_groups is not None else self.config.num_beam_groups
@@ -92,9 +138,45 @@ class Data2TextGenerator(GenerationMixin):
 
         encoder_input_ids = input_ids if self.config.is_encoder_decoder else None
 
+        if self.config.is_encoder_decoder:
+            # add encoder_outputs to model_kwargs
+            model_kwargs = self._prepare_encoder_decoder_kwargs_for_generation(input_ids_col0, 
+                                                                                input_ids_col1,
+                                                                                input_ids_col2,
+                                                                                input_ids_col3,
+                                                                                input_ids_col4,
+                                                                                 model_kwargs)
+
+            if "decoder_input_ids" in model_kwargs:
+                input_ids = model_kwargs.pop("decoder_input_ids")
+            else:
+                input_ids = self._prepare_decoder_input_ids_for_generation(
+                    input_ids, decoder_start_token_id=decoder_start_token_id, bos_token_id=bos_token_id
+                )
+
+            print("INPUT IDS", input_ids)
+            print("MODEL KWARGS", model_kwargs)
 
 if __name__ == '__main__':
-    model = LitModel.load_from_checkpoint(checkpoint_path="webnlg_sanity_model.ckpt")
     tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
+    model = LitModel.load_from_checkpoint(checkpoint_path="webnlg_sanity_model.ckpt")
+    '''bart_model = BartForDataToText.from_pretrained('facebook/bart-base')    
+    hparams = argparse.Namespace()
+
+    hparams.freeze_encoder = True
+    hparams.freeze_embeds = True
+    hparams.eval_beams = 4
+
+    model = LitModel(learning_rate = 1e-5, tokenizer = tokenizer, model = bart_model, hparams = hparams)'''
+    summary_data = SummaryDataModule(tokenizer, data_files = ['/Users/sanjana/roboreviewer_summarization/data/web_nlg_train.csv', 
+                                           '/Users/sanjana/roboreviewer_summarization/data/web_nlg_test.csv', 
+                                           '/Users/sanjana/roboreviewer_summarization/data/web_nlg_dev.csv'], batch_size = 1)
+    summary_data.prepare_data()
+    summary_data.setup("stage")
+    train_data = summary_data.train_dataloader()
+
+    it = iter(train_data)
+    first_batch = next(it)
     generator = Data2TextGenerator(model, tokenizer)
-    generator.generate()
+
+    generator.generate(first_batch)
