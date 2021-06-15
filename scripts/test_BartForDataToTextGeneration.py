2882683784028af1c0e5c1c5dd64cf2e9cb08d9f
sanjanaramprasad <sanjanaramprasad1@gmail.com>
2021-06-15 14:46:39 -0400

    BartForDataToText

--- scripts/test_BartForDataToTextGeneration.py
+++ scripts/test_BartForDataToTextGeneration.py
@@ -2,40 +2,19 @@
 from Data2TextProcessor_1 import SummaryDataModule
 from transformers import BartTokenizer, BartForCausalLM, BartForConditionalGeneration, BeamSearchScorer, LogitsProcessorList, MinLengthLogitsProcessor, TopKLogitsWarper, TemperatureLogitsWarper
 import torch.optim as optim
-#from BartForDataToTextGeneration import BartForDataToText
-#from Data2TextProcessor_1 import SummaryDataModule
 
-from BartForDataToTextGeneration_decoder_mod import BartForDataToTextDecoderMod
-#from BartForDataToTextGeneration_addition import BartForDataToText
+
 
 from torch import nn 
 import torch
-#additional_special_tokens = []
 
-additional_special_tokens=[ "<sep>"]
-'''model = BartForDataToText.from_pretrained('facebook/bart-base')
 
-#model._make_duplicate_encoders()
-tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
-tokenizer.add_tokens(additional_special_tokens)
-#model.resize_token_embeddings(len(tokenizer))
-model._make_duplicate_encoders()
-model.resize_token_embeddings(len(tokenizer))
-summary_data = SummaryDataModule(tokenizer, data_files = ['/home/sanjana/roboreviewer_summarization/data/robo_train_sep.csv', 
-                                           '/home/sanjana/roboreviewer_summarization/data/robo_dev_sep.csv', 
-                                           '/home/sanjana/roboreviewer_summarization/data/robo_test_sep.csv'], batch_size = 1)
-summary_data.prepare_data()
 
-summary_data.setup("stage")
-test_data = summary_data.test_dataloader(data_type = 'robo')
-it = iter(test_data)
-'''
-
-def make_data(tokenizer, data_type = 'robo', path = '/home/sanjana'):
+def make_data(tokenizer, data_type = 'robo', path = '/home/sanjana', files = ['robo_train_sep.csv', 'robo_dev_sep.csv', 'robo_test_sep.csv']):
     if data_type == 'robo':
-        train_file = path + '/roboreviewer_summarization/data/robo_train_sep.csv'
-        dev_file = path + '/roboreviewer_summarization/data/robo_dev_sep.csv'
-        test_file = path + '/roboreviewer_summarization/data/robo_test_sep.csv'
+        train_file = path + '/roboreviewer_summarization/data/%s'%(files[0])
+        dev_file = path + '/roboreviewer_summarization/data/%s'%(files[1])
+        test_file = path + '/roboreviewer_summarization/data/%s'%(files[2])
 
     elif data_type =='webnlg':
         train_file = path + '/roboreviewer_summarization/data/web_nlg_train.csv'
@@ -48,7 +27,28 @@ def make_data(tokenizer, data_type = 'robo', path = '/home/sanjana'):
     return summary_data
 
 
-additional_special_tokens=["<attribute>",  "</attribute>", "<sep>"]
+def get_data(data):
+        input_ids_col0 = data[0] if len(data) >1 else None
+        attention_mask_col0 = data[1] if len(data) >1 else None
+
+        input_ids_col1 = data[2] if len(data) >3 else None
+        attention_mask_col1 = data[3] if len(data) >3 else None
+
+        input_ids_col2 = data[4] if len(data) >5 else None
+        attention_mask_col2 = data[5] if len(data) >5 else None
+
+        input_ids_col3 = data[6] if len(data) >7 else None
+        attention_mask_col3 = data[7] if len(data) >7 else None
+
+        input_ids_col4 = data[8] if len(data) >9 else None
+        attention_mask_col4 = data[9] if len(data) >9 else None
+
+        return input_ids_col0, attention_mask_col0, \
+            input_ids_col1, attention_mask_col1, \
+            input_ids_col2, attention_mask_col2, \
+            input_ids_col3, attention_mask_col3, \
+            input_ids_col4, attention_mask_col4
+
 additional_special_tokens = ["<sep>",
             "<outcomes>", "</outcomes>",
             "<punchline_text>", "</punchline_text>",
@@ -57,17 +57,9 @@ additional_special_tokens = ["<sep>",
 tokenizer = BartTokenizer.from_pretrained('facebook/bart-base', bos_token="<s>",
                                                     eos_token="</s>",
                                                     pad_token = "<pad>")
-#tokenizer.add_tokens(additional_special_tokens)
-model = BartForDataToTextDecoderMod.from_pretrained('facebook/bart-base')
-model._make_duplicate_encoders(layer_share = False)
-model._make_duplicate_decoder_layer_attns()
-model.resize_token_embeddings(len(tokenizer))
-print("Loading Data ...")
-summary_data = make_data(tokenizer, path = '/home/sanjana')
-summary_data.setup("stage")
-test_data = summary_data.test_dataloader(data_type = 'robo')
-print("Done.")
-it = iter(test_data)
+
+
+
 
 class BartForDataToTextGenerationTester():
 
@@ -168,23 +160,27 @@ class BartForDataToTextGenerationTester():
         attention_added = model._get_attention_masks_OR(self.attn_list)
         print(attention_added.shape)
 
-    def test_model_forward(self):
-   
+    
+
+    def test_model_forward_bart_encoder_loop_per_study(self):
+        from BartForDataToTextGeneration_encoder_combination import BartForDataToText
+        from Data2TextProcessor_loop import SummaryDataModule
+        model = BartForDataToText.from_pretrained('facebook/bart-base')
+        model._make_duplicate_encoders(layer_share = False)
+        model.resize_token_embeddings(len(tokenizer))
+        print("Loading Data ...")
+        summary_data = make_data(tokenizer, path = '/home/sanjana', files = ['robo_train_linearized_per_study.csv', 
+                            'robo_dev_linearized_per_study.csv', 'robo_test_linearized_per_study'])
+        summary_data.setup("stage")
+        test_data = summary_data.test_dataloader(data_type = 'robo')
+        print("Done.")
+        it = iter(test_data)
+        
         data = next(it)
-        input_ids_col0 = data[0] if len(data) >1 else None
-        attention_mask_col0 = data[1] if len(data) >1 else None
-
-        input_ids_col1 = data[2] if len(data) >3 else None
-        attention_mask_col1 = data[3] if len(data) >3 else None
-
-        input_ids_col2 = data[4] if len(data) >5 else None
-        attention_mask_col2 = data[5] if len(data) >5 else None
-
-        input_ids_col3 = data[6] if len(data) >7 else None
-        attention_mask_col3 = data[7] if len(data) >7 else None
-
-        input_ids_col4 = data[8] if len(data) >9 else None
-        attention_mask_col4 = data[9] if len(data) >9 else None
+        input_ids_col0, attention_mask_col0, input_ids_col1, attention_mask_col1, \
+            input_ids_col2, attention_mask_col2, \
+            input_ids_col3, attention_mask_col3, \
+            input_ids_col4, attention_mask_col4 = get_data(data)
 
         print("forward...") 
         outputs = model(
@@ -199,7 +195,8 @@ class BartForDataToTextGenerationTester():
             attention_mask_col3 = attention_mask_col3,
             attention_mask_col4 = attention_mask_col4,
             labels = data[6],
-            encoder_combination_type = 'hierarch',
+            encoder_forward_startegy = 'single',
+            encoder_combination_type = 'addition',
             use_cache = False
         )
         tgt_ids = data[-1]
@@ -213,48 +210,13 @@ class BartForDataToTextGenerationTester():
         print('=' *13)
 
 
-def make_data(tokenizer, data_type = 'robo', path = '/home/sanjana'):
-    if data_type == 'robo':
-        train_file = path + '/roboreviewer_summarization/data/robo_train_sep.csv'
-        dev_file = path + '/roboreviewer_summarization/data/robo_dev_sep.csv'
-        test_file = path + '/roboreviewer_summarization/data/robo_test_sep.csv'
 
-    elif data_type =='webnlg':
-        train_file = path + '/roboreviewer_summarization/data/web_nlg_train.csv'
-        dev_file = path + '/roboreviewer_summarization/data/web_nlg_dev.csv'
-        test_file = path + '/roboreviewer_summarization/data/web_nlg_test.csv'
 
-    data_files = [train_file, dev_file, test_file]
-    summary_data = SummaryDataModule(tokenizer, data_files = data_files,  batch_size = 1)
-    summary_data.prepare_data()
-    return summary_data
-
-'''
-additional_special_tokens=["<attribute>",  "</attribute>", "<sep>"]
-tokenizer = BartTokenizer.from_pretrained('facebook/bart-base', bos_token="<s>",
-                                                    eos_token="</s>",
-                                                    pad_token = "<pad>")
-tokenizer.add_tokens(additional_special_tokens)
-bart_model = BartForDataToText.from_pretrained('facebook/bart-base')
-bart_model._make_duplicate_encoders()
-bart_model.model.resize_token_embeddings(len(tokenizer))
-summary_data = make_data(tokenizer, path = '/home/sanjana')
-
-    
-'''
-    
+ 
         
 obj = BartForDataToTextGenerationTester()
-#obj.test_get_encoders()
-#obj.test_encoder0()
-#obj.test_encoder1()
-#obj.test_encoder2()
-#obj.test_encoder3()
-#obj.test_encoder4()
-#obj.test_encoder_concat()
-#obj.test_encoder_addition()
-#obj.test_attn_masks_OR()
-obj.test_model_forward()
+
+obj.test_model_forward_bart_encoder_loop_per_study()
 
 #print(obj.encoder_col3.layers[0].final_layer_norm.weight.grad==obj.encoder_col4.layers[0].final_layer_norm.weight.grad)
 #print(obj.encoder_col3.layers[-1].final_layer_norm.weight.grad ==obj.encoder_col4.layers[-1].final_layer_norm.weight)
