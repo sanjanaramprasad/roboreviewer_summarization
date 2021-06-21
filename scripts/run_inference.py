9f75351194db7523e0a78949ba527bfd41d0773f
sanjanaramprasad <sanjanaramprasad1@gmail.com>
2021-06-21 17:18:48 -0400

    inference

--- scripts/run_inference.py
+++ scripts/run_inference.py
@@ -146,39 +146,64 @@ class Data2TextGenerator(GenerationMixin):
                 encoder_kwargs = {argument: value for argument, value in model_kwargs.items() if not "col" in argument}
                 attention_mask_col0 = encoder_kwargs.get("attention_mask_col0", None)
                 encoder_outputs = encoder_kwargs.get('encoder_outputs_col0', None)
+
                 if model_kwargs["encoder_forward_stratergy"] == 'single':
                     model_kwargs["encoder_outputs_col0"]: ModelOutput = self.model._get_encoder_outputs(encoder = encoder_col0, encoder_outputs = encoder_outputs, input_ids = input_ids_col0, attention_mask = attention_mask_col0)
                 else:
-                    model_kwargs["encoder_outputs_col0"] , _ = self.model.self.__get_loop_encoder_outputs(encoder_col0, encoder_outputs, input_ids_col0, \
-                        attention_mask_col0, inc_count = 256, enc_ind = "0")
+                    model_kwargs["encoder_outputs_col0"] , _ = self.model._loop_encoders(encoder_col0, encoder_outputs, input_ids_col0, \
+                        attention_mask_col0, inc_count = 256, fc0 = self.model.fc0_enc0, fc1 = self.model.fc1_enc0, final_layer = self.model.final_layer_enc0)
 
             if model_kwargs["encoder_forward_stratergy"] == 'single':
                  if not(input_ids_col1 is None):
-                     encoder_kwargs = {argument: value for argument, value in model_kwargs.items() if not "col" in argument}
-                     attention_mask_col1 = encoder_kwargs.get("attention_mask_col1", None)
-                     encoder_outputs = encoder_kwargs.get('encoder_outputs_col1', None)
-                     model_kwargs["encoder_outputs_col1"]: ModelOutput = self.model._get_encoder_outputs(encoder = encoder_col1, encoder_outputs = encoder_outputs, input_ids = input_ids_col1, attention_mask = attention_mask_col1)
+                    encoder_kwargs = {argument: value for argument, value in model_kwargs.items() if not "col" in argument}
+                    attention_mask_col1 = encoder_kwargs.get("attention_mask_col1", None)
+                    encoder_outputs = encoder_kwargs.get('encoder_outputs_col1', None)
+
+                    if model_kwargs["encoder_forward_stratergy"] == 'single':
+                        model_kwargs["encoder_outputs_col1"]: ModelOutput = self.model._get_encoder_outputs(encoder = encoder_col1, encoder_outputs = encoder_outputs, input_ids = input_ids_col1, attention_mask = attention_mask_col1)
+                    else:
+                        model_kwargs["encoder_outputs_col1"] , _ = self.model._loop_encoders(encoder_col1, encoder_outputs, input_ids_col1, \
+                            attention_mask_col1, inc_count = 256, fc0 = self.model.fc0_enc1, fc1 = self.model.fc1_enc1, final_layer = self.model.final_layer_enc1)
 
             if model_kwargs["encoder_forward_stratergy"] == 'single':
                  if not(input_ids_col2 is None):
-                     encoder_kwargs = {argument: value for argument, value in model_kwargs.items() if not "col" in argument}
-                     attention_mask_col2 = encoder_kwargs.get("attention_mask_col2", None)
-                     encoder_outputs = encoder_kwargs.get('encoder_outputs_col2', None)
-                     model_kwargs["encoder_outputs_col2"]: ModelOutput = self.model._get_encoder_outputs(encoder = encoder_col2, encoder_outputs = encoder_outputs, input_ids = input_ids_col2, attention_mask = attention_mask_col2)
+                    encoder_kwargs = {argument: value for argument, value in model_kwargs.items() if not "col" in argument}
+                    attention_mask_col2 = encoder_kwargs.get("attention_mask_col2", None)
+                    encoder_outputs = encoder_kwargs.get('encoder_outputs_col2', None)
+
+                    if model_kwargs["encoder_forward_stratergy"] == 'single':
+                        model_kwargs["encoder_outputs_col2"]: ModelOutput = model_kwargs["encoder_outputs_col2"]: ModelOutput = self.model._get_encoder_outputs(encoder = encoder_col2, encoder_outputs = encoder_outputs, input_ids = input_ids_col2, attention_mask = attention_mask_col2)
+                    else:
+                        model_kwargs["encoder_outputs_col2"] , _ = self.model._loop_encoders(encoder_col2, encoder_outputs, input_ids_col2, \
+                            attention_mask_col2, inc_count = 256, fc0 = self.model.fc0_enc2, fc1 = self.model.fc1_enc2, final_layer = self.model.final_layer_enc2)
+
+                     
 
             if model_kwargs["encoder_forward_stratergy"] == 'single':
                  if not(input_ids_col3 is None):
-                     encoder_kwargs = {argument: value for argument, value in model_kwargs.items() if not "col" in argument}
-                     attention_mask_col3 = encoder_kwargs.get("attention_mask_col0", None)
-                     encoder_outputs = encoder_kwargs.get('encoder_outputs_col3', None)
-                     model_kwargs["encoder_outputs_col3"]: ModelOutput = self.model._get_encoder_outputs(encoder = encoder_col3, encoder_outputs = encoder_outputs, input_ids = input_ids_col3, attention_mask = attention_mask_col3)
+                    encoder_kwargs = {argument: value for argument, value in model_kwargs.items() if not "col" in argument}
+                    attention_mask_col3 = encoder_kwargs.get("attention_mask_col3", None)
+                    encoder_outputs = encoder_kwargs.get('encoder_outputs_col3', None)
+                    
+                    if model_kwargs["encoder_forward_stratergy"] == 'single':
+                        model_kwargs["encoder_outputs_col3"]: ModelOutput = self.model._get_encoder_outputs(encoder = encoder_col3, encoder_outputs = encoder_outputs, input_ids = input_ids_col3, attention_mask = attention_mask_col3)
+                    else:
+                        model_kwargs["encoder_outputs_col3"] , _ = self.model._loop_encoders(encoder_col3, encoder_outputs, input_ids_col3, \
+                            attention_mask_col3, inc_count = 256, fc0 = self.model.fc0_enc3, fc1 = self.model.fc1_enc3, final_layer = self.model.final_layer_enc3)
+
+                     
 
             if model_kwargs["encoder_forward_stratergy"] == 'single':
                  if not(input_ids_col4 is None):
-                     encoder_kwargs = {argument: value for argument, value in model_kwargs.items() if not "col" in argument}
-                     attention_mask_col0 = encoder_kwargs.get("attention_mask_col4", None)
-                     encoder_outputs = encoder_kwargs.get('encoder_outputs_col4', None)
-                     model_kwargs["encoder_outputs_col4"]: ModelOutput = self.model._get_encoder_outputs(encoder = encoder_col4, encoder_outputs = encoder_outputs, input_ids = input_ids_col4, attention_mask = attention_mask_col4)
+                    encoder_kwargs = {argument: value for argument, value in model_kwargs.items() if not "col" in argument}
+                    attention_mask_col4 = encoder_kwargs.get("attention_mask_col4", None)
+                    encoder_outputs = encoder_kwargs.get('encoder_outputs_col4', None)
+
+                    if model_kwargs["encoder_forward_stratergy"] == 'single':
+                        model_kwargs["encoder_outputs_col4"]: ModelOutput = model_kwargs["encoder_outputs_col4"]: ModelOutput = self.model._get_encoder_outputs(encoder = encoder_col4, encoder_outputs = encoder_outputs, input_ids = input_ids_col4, attention_mask = attention_mask_col4)
+                    else:
+                        model_kwargs["encoder_outputs_col2"] , _ = self.model._loop_encoders(encoder_col4, encoder_outputs, input_ids_col4, \
+                            attention_mask_col4, inc_count = 256, fc0 = self.model.fc0_enc4, fc1 = self.model.fc1_enc4, final_layer = self.model.final_layer_enc4)
 
         return model_kwargs
         
