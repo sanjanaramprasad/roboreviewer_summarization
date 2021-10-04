affd69c408562ab6e72c07b38b50d4843a9aa29c
sanjanaramprasad <sanjanaramprasad1@gmail.com>
2021-10-04 13:13:16 -0400

    multi LM

---
+++ scripts/bart_content_select/BartForDataToTextGeneration_lm.py
@@ -0,0 +1,197 @@
+import torch
+from torch import nn
+from transformers.models.bart.modeling_bart import BartEncoder, BartModel, BartPretrainedModel, shift_tokens_right, BartDecoderLayer, BartLearnedPositionalEmbedding,  _make_causal_mask, _expand_mask
+from transformers.models.bart.configuration_bart import BartConfig
+from transformers.modeling_outputs import BaseModelOutput,Seq2SeqLMOutput,Seq2SeqModelOutput, Seq2SeqQuestionAnsweringModelOutput,Seq2SeqSequenceClassifierOutput
+from transformers.modeling_utils import PreTrainedModel
+from torch.nn import CrossEntropyLoss, MSELoss
+import copy
+import torch.nn.functional as F
+from typing import Optional, Tuple
+from transformers.activations import ACT2FN
+from BartForDataToTextGeneration_encoder_combination import BartForDataToText
+import random
+from transformers.modeling_outputs import (
+    BaseModelOutput,
+    BaseModelOutputWithPastAndCrossAttentions,
+    CausalLMOutputWithCrossAttentions,
+    Seq2SeqLMOutput,
+    Seq2SeqModelOutput,
+    Seq2SeqQuestionAnsweringModelOutput,
+    Seq2SeqSequenceClassifierOutput,
+)
+
+
+class BartForDataToTextGeneration_MultiLM(BartPretrainedModel):
+    base_model_prefix = "model"
+    _keys_to_ignore_on_load_missing = [r"final_logits_bias", r"lm_head\.weight"]
+
+    def __init__(self, config: BartConfig):
+        super().__init__(config)
+        self.model = BartModel(config)
+        self.register_buffer("final_logits_bias0", torch.zeros((1, self.model.shared.num_embeddings)))
+        self.register_buffer("final_logits_bias1", torch.zeros((1, self.model.shared.num_embeddings)))
+        self.register_buffer("final_logits_bias2", torch.zeros((1, self.model.shared.num_embeddings)))
+        self.lm_head0 = nn.Linear(config.d_model, self.model.shared.num_embeddings, bias=False)
+        self.lm_head1 = nn.Linear(config.d_model, self.model.shared.num_embeddings, bias=False)
+        self.lm_head2 = nn.Linear(config.d_model, self.model.shared.num_embeddings, bias=False)
+
+        self.init_weights()
+
+    def get_encoder(self):
+        return self.model.get_encoder()
+
+    def get_decoder(self):
+        return self.model.get_decoder()
+
+    def resize_token_embeddings(self, new_num_tokens: int) -> nn.Embedding:
+        new_embeddings = super().resize_token_embeddings(new_num_tokens)
+        self._resize_final_logits_bias(new_num_tokens)
+        return new_embeddings
+
+    def _resize_final_logits_bias(self, new_num_tokens: int) -> None:
+        old_num_tokens = self.final_logits_bias.shape[-1]
+        if new_num_tokens <= old_num_tokens:
+            new_bias = self.final_logits_bias[:, :new_num_tokens]
+        else:
+            extra_bias = torch.zeros((1, new_num_tokens - old_num_tokens), device=self.final_logits_bias.device)
+            new_bias = torch.cat([self.final_logits_bias, extra_bias], dim=1)
+        self.register_buffer("final_logits_bias", new_bias)
+
+    def get_output_embeddings(self):
+        return self.lm_head
+
+    def set_output_embeddings(self, new_embeddings):
+        self.lm_head0 = new_embeddings
+        self.lm_head1 = new_embeddings
+        self.lm_head2 = new_embeddings
+
+    def resize_token_embeddings(self, new_num_tokens: int) -> nn.Embedding:
+        new_embeddings = super().resize_token_embeddings(new_num_tokens)
+        self._resize_final_logits_bias(new_num_tokens)
+        return new_embeddings
+
+
+    def forward(
+        self,
+        input_ids_col0 = None,
+        input_ids_col1 = None,
+        input_ids_col2 = None, 
+        attention_mask_col0 = None,
+        attention_mask_col1 = None,
+        attention_mask_col2 = None,
+        decoder_input_ids=None,
+        decoder_attention_mask=None,
+        head_mask=None,
+        decoder_head_mask=None,
+        encoder_outputs_col0 = None,
+        encoder_outputs_col1 = None,
+        encoder_outputs_col2 = None,
+        past_key_values=None,
+        inputs_embeds=None,
+        decoder_inputs_embeds=None,
+        cross_attn_head_mask=None,
+        labels=None,
+        use_cache=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+
+
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+                
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        
+        use_cache = use_cache if use_cache is not None else self.config.use_cache
+        
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        if labels is not None:
+            if decoder_input_ids is None and decoder_inputs_embeds is None:
+                decoder_input_ids = shift_tokens_right(
+                    labels, self.config.pad_token_id, self.config.decoder_start_token_id
+                )
+
+        outputs0 = self.model(
+            input_ids_col0,
+            attention_mask=attention_mask_col0,
+            decoder_input_ids=decoder_input_ids,
+            encoder_outputs=encoder_outputs_col0,
+            decoder_attention_mask=decoder_attention_mask,
+            head_mask=head_mask,
+            decoder_head_mask=decoder_head_mask,
+            cross_attn_head_mask=cross_attn_head_mask,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            decoder_inputs_embeds=decoder_inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        outputs1 = self.model(
+            input_ids_col1,
+            attention_mask=attention_mask_col1,
+            decoder_input_ids=decoder_input_ids,
+            encoder_outputs=encoder_outputs_col1,
+            decoder_attention_mask=decoder_attention_mask,
+            head_mask=head_mask,
+            decoder_head_mask=decoder_head_mask,
+            cross_attn_head_mask=cross_attn_head_mask,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            decoder_inputs_embeds=decoder_inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        outputs2 = self.model(
+            input_ids_col2,
+            attention_mask=attention_mask_col2,
+            decoder_input_ids=decoder_input_ids,
+            encoder_outputs=encoder_outputs_col2,
+            decoder_attention_mask=decoder_attention_mask,
+            head_mask=head_mask,
+            decoder_head_mask=decoder_head_mask,
+            cross_attn_head_mask=cross_attn_head_mask,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            decoder_inputs_embeds=decoder_inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        lm_logits0 = self.lm_head(outputs0[0]) + self.final_logits_bias0
+        lm_logits1 = self.lm_head(outputs1[0]) + self.final_logits_bias1
+        lm_logits2 = self.lm_head(outputs2[0]) + self.final_logits_bias2
+
+        masked_lm_loss = None
+        if labels is not None:
+            loss_fct = CrossEntropyLoss()
+            masked_lm_loss = loss_fct(lm_logits0.view(-1, self.config.vocab_size), labels.view(-1))
+
+        if not return_dict:
+            output = (lm_logits0,) + outputs0[1:]
+            return ((masked_lm_loss,) + output) if masked_lm_loss is not None else output
+
+        return Seq2SeqLMOutput(
+            loss=masked_lm_loss,
+            logits=lm_logits0,
+            past_key_values=outputs0.past_key_values,
+            decoder_hidden_states=outputs0.decoder_hidden_states,
+            decoder_attentions=outputs0.decoder_attentions,
+            cross_attentions=outputs0.cross_attentions,
+            encoder_last_hidden_state=outputs0.encoder_last_hidden_state,
+            encoder_hidden_states=outputs0.encoder_hidden_states,
+            encoder_attentions=outputs0.encoder_attentions,
+        )
--- scripts/bart_content_select/check_model.py
+++ scripts/bart_content_select/check_model.py
@@ -108,10 +108,59 @@ class BartForDataToTextGenerationTester():
         print('=' *13)
 
 
+    def test_model_forward_bart_multiLM(self):
+        from BartForDataToTextGeneration_lm import BartForDataToTextGeneration_MultiLM
+        #from Data2TextProcessor_1 import SummaryDataModule
+        model = BartForDataToTextGeneration_MultiLM.from_pretrained('facebook/bart-base')
+        model.resize_token_embeddings(len(tokenizer))
+        print("Loading Data ...")
+        data_files = ['train_rr_data.csv', 'dev_rr_data.csv' , 'test_rr_data.csv']
+        #summary_data = make_data(tokenizer, SummaryDataModule, data_type = 'robo', path = '/home/sanjana', files = data_files, max_len = 1024)
+    
+        #summary_data.setup("stage")
+        #test_data = summary_data.test_dataloader(data_type = 'robo')
+        
+        summary_data = make_data(tokenizer, SummaryDataModule, data_type = 'robo', path = '/home/sanjana', files = data_files, max_len = 1024)
+        print(summary_data.train)
+        summary_data.setup("stage")
+        it = summary_data.val_dataloader()
+        #batches = iter(it)
+        #batch = next(batches)
+        print("Done.")
+        it = iter(it)
+        
+        data = next(it)
+        input_ids_col0, attention_mask_col0, input_ids_col1, attention_mask_col1, \
+            input_ids_col2, attention_mask_col2 = get_data(data)
+
+        
+
+        print("forward...") 
+        outputs = model(
+            input_ids_col0 = input_ids_col0,
+            input_ids_col1 = input_ids_col1,
+            input_ids_col2 = input_ids_col2, 
+            attention_mask_col0 = attention_mask_col0,
+            attention_mask_col1 = attention_mask_col1,
+            attention_mask_col2 = attention_mask_col2,
+            labels = data[-1],
+            use_cache = True
+        )
+        tgt_ids = data[-1]
+        optimizer = optim.Adam(model.parameters())
+        loss = outputs[0]
+        #ce_loss_fct = torch.nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id)
+        #loss = ce_loss_fct(lm_logits.view(-1, lm_logits.shape[-1]), tgt_ids.view(-1))
+        optimizer.zero_grad()
+        loss.backward()
+        print("OUTPUTS", outputs[0])
+        print('=' *13)
+
+
 
 
  
         
 obj = BartForDataToTextGenerationTester()
-obj.test_model_forward_bart_decoder_addition()
+obj.test_model_forward_bart_multiLM()
 #obj.test_model_forward_bart_encoder_loop_per_study()
