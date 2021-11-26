245d3ef5216c934f3e0cd0056c6f16db3b85568a
sanjanaramprasad <sanjanaramprasad1@gmail.com>
2021-11-25 19:11:37 -0500

    modeling framework

--- scripts/bart_multi_lm/model.py
+++ scripts/bart_multi_lm/model.py
@@ -171,19 +171,25 @@ class BartForDataToTextGeneration_MultiLM(BartPretrainedModel):
                         #print(enc_last_hs_vectors[i].tolist())
                 batch_vector_list += sentence_output
             vector_list.append(batch_vector_list)
-        print("VECTOR LIST", len(vector_list))
-        vector_list_pad = [0] * embed_dim
-        vector_attn_pad = [0] * (max_len - len(vector_list))
-        vector_attention = [1] * len(vector_list)
 
-        vector_list += [vector_list_pad] * (max_len - len(vector_list))
-        vector_attention += vector_attn_pad
-        print(len(vector_attn_pad), len(vector_list))
-        vector_list = torch.as_tensor([vector_list], device = encoder_output_list[0][0].device)
+        vector_list_padded = []
+        vector_attentions = []
+        for vect_list in vector_list:
+            print("VECTOR LIST", len(vect_list))
+            vector_list_pad = [0] * embed_dim
+            vector_attn_pad = [0] * (max_len - len(vect_list))
+            vector_attention = [1] * len(vect_list)
+
+            vector_list_padded.append([vector_list_pad] * (max_len - len(vect_list)))
+            vector_attention += vector_attn_pad
+            print(len(vector_attn_pad), len(vector_list))
+            vector_list_pad.append(vect_list)
+            vector_attentions.append(vector_attention)
+        vector_list = torch.as_tensor([vector_list_padded], device = encoder_output_list[0][0].device)
         #vector_attention = [1] * len(vector_list)
-        vector_attention = torch.as_tensor([vector_attention], device = encoder_output_list[0][0].device)
-        print("SENT VECT,  SENT ATTN", vector_list.shape, vector_attention.shape)
-        return vector_list, vector_attention
+        vector_attentions = torch.as_tensor([vector_attentions], device = encoder_output_list[0][0].device)
+        print("SENT VECT,  SENT ATTN", vector_list.shape, vector_attentions.shape)
+        return vector_list, vector_attentions
 
     def forward(
         self,
